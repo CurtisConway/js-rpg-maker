6b3b4b08aaa613b70a888103567b59229fc0ec99
Curtis <curtis@drumeo.com>
2019-12-29 17:34:35 -0800

    Refactor scene class with tests, can mount, unmount and move on to the next scene

--- src/scene.js
+++ src/scene.js
@@ -1,5 +1,66 @@
-export default class Scene {
-    constructor() {
-        this.isConstructed = true;
+import Events from './utils/event';
+
+/**
+ * @class Scene
+ *
+ * @mixes Events
+ */
+class Scene {
+    /**
+     * Mount the scene
+     *
+     * @param {object} params - event handler for the mount event
+     * @param {function(Object)} [params.onMount] - event handler for the mount event
+     * @param {function(Object)} [params.onDismount] - event handler for the dismount event
+     *
+     * @constructor
+     */
+    constructor({
+        onMount,
+        onDismount,
+    }) {
+        this.isMounted = false;
+        this.eventHandlers = {
+            mount: onMount ? [onMount] : [],
+            dismount: onDismount ? [onDismount] : [],
+        };
+
+        (async () => {
+            await this.mount();
+        })();
+    }
+
+    /**
+     * Mount the scene
+     */
+    async mount() {
+        setTimeout(() => {
+            this.isMounted = true;
+            this.emit('mount');
+        }, 50);
+    }
+
+    /**
+     * Dismount the scene
+     */
+    async dismount() {
+        this.isMounted = false;
+        this.emit('dismount');
+    }
+
+    /**
+     * Dismount the scene and move on to the next scene
+     */
+    async nextScene(scene) {
+        await this.dismount();
+
+        return scene;
     }
 }
+
+/**
+ * Assign the Events mixin
+ */
+Object.assign(Scene.prototype, Events);
+
+export default Scene;
--- tests/scene.test.js
+++ tests/scene.test.js
@@ -1,13 +1,57 @@
 import Scene from '../src/scene';
 
 describe('scene', () => {
-    let scene;
+    it('can add an onMount handler', () => {
+        const scene = new Scene({
+            onMount: () => ({})
+        });
 
-    beforeEach(() => {
-        scene = new Scene();
+        expect(scene.eventHandlers.mount.length).toBeGreaterThan(0);
     });
 
-    it('constructs', () => {
-        expect(scene.isConstructed).toBe(true);
+    it('can add an onDismount handler', () => {
+        const scene = new Scene({
+            onDismount: () => ({})
+        });
+
+        expect(scene.eventHandlers.dismount.length).toBeGreaterThan(0);
+    });
+
+    it('can mount', (done) => {
+        const scene = new Scene({
+            onMount: () => {
+                expect(scene.isMounted).toBe(true);
+
+                done();
+            }
+        });
+    });
+
+    it('can dismount', (done) => {
+        const scene = new Scene({
+            onMount: () => {
+                scene.dismount();
+            },
+            onDismount: () => {
+                expect(scene.isMounted).toBe(false);
+
+                done();
+            },
+        });
+    });
+
+    it('can move on to the next scene', (done) => {
+        const sceneOne = new Scene({
+            onMount: async () => {
+                const sceneTwo = await sceneOne.nextScene(new Scene({
+                    onMount: () => {
+                        expect(sceneOne.isMounted).toBe(false);
+                        expect(sceneTwo.isMounted).toBe(true);
+
+                        done();
+                    }
+                }));
+            }
+        });
     });
 });
