3f160f0e6a19548b480f627caf1f0a4ea832e424
Curtis <curtis@drumeo.com>
2019-12-29 19:15:10 -0800

    Add unit and tests

---
+++ src/unit/unit.js
@@ -0,0 +1,118 @@
+import Events from '../utils/event';
+
+/**
+ * @class Unit
+ *
+ * @mixes Events
+ */
+class Unit {
+    /**
+     * Construct the Unit
+     *
+     * @param {object} params
+     * @param {string} params.name - the name of the unit
+     * @param {number} [params.totalHealth=1] - the total health of the unit
+     * @param {number} [params.currentHealth] - the current health of the unit
+     * @param {number} [params.speed=1] - the speed of the unit
+     * @param {number} [params.strength=1] - the strength of the unit
+     * @param {number} [params.armor=1] - the armor of the unit
+     * @param {function(Object)} [params.onSpawn] - event handler for the spawn event
+     * @param {function(Object)} [params.onDeath] - event handler for the death event
+     *
+     * @constructor
+     */
+    constructor({
+        name,
+        totalHealth = 1,
+        currentHealth = 0,
+        speed = 1,
+        strength = 1,
+        armor = 1,
+        onSpawn,
+        onDeath,
+    }) {
+        this.name = name;
+        this.alive = currentHealth > 0;
+        this.eventHandlers = {
+            spawn: onSpawn ? [onSpawn] : [],
+            death: onDeath ? [onDeath] : [],
+        };
+
+        this.spawn({
+            name,
+            totalHealth,
+            currentHealth,
+            armor,
+            speed,
+            strength,
+        });
+    }
+
+    /**
+     * Spawn the Unit
+     *
+     * @param {object} params
+     * @param {number} params.totalHealth - the total health of the unit
+     * @param {number} params.currentHealth - the current health of the unit
+     * @param {number} params.speed - the speed of the unit
+     * @param {number} params.strength - the strength of the unit
+     * @param {number} params.armor - the armor of the unit
+     */
+    spawn({
+        totalHealth,
+        currentHealth,
+        speed,
+        strength,
+        armor,
+    }) {
+        this.attributes = {
+            totalHealth,
+            armor,
+            speed,
+            strength,
+        };
+
+        if (currentHealth != null && currentHealth <= totalHealth) {
+            this.attributes.currentHealth = Number(currentHealth);
+        } else {
+            this.attributes.currentHealth = Number(totalHealth);
+        }
+
+        this.emit('spawn');
+    }
+
+    /**
+     * Damage the Unit
+     *
+     * @param {number} amount
+     */
+    damage(amount = 0) {
+        this.attributes.currentHealth -= amount;
+
+        if (this.attributes.currentHealth <= 0) {
+            this.alive = false;
+            this.emit('death');
+        }
+    }
+
+
+    /**
+     * Damage the Unit
+     *
+     * @param {number} amount
+     */
+    heal(amount = 0) {
+        this.attributes.currentHealth += amount;
+
+        if (this.attributes.currentHealth > this.attributes.totalHealth) {
+            this.attributes.currentHealth = this.attributes.totalHealth;
+        }
+    }
+}
+
+/**
+ * Assign the Events mixin
+ */
+Object.assign(Unit.prototype, Events);
+
+export default Unit;
---
+++ tests/unit.test.js
@@ -0,0 +1,72 @@
+import Unit from '../src/unit/unit';
+
+describe('Unit', () => {
+    let unit;
+
+    beforeEach(() => {
+        unit = new Unit({
+            name: 'Test Unit',
+            totalHealth: 100,
+            currentHealth: 60,
+            armor: 100,
+            speed: 10,
+            strength: 10,
+            onSpawn: () => ({}),
+            onDeath: () => ({}),
+        });
+    });
+
+    it('can have a name', () => {
+        expect(unit.name).toBe('Test Unit');
+    });
+
+    it('has a total health', () => {
+        expect(unit.attributes.totalHealth).toBe(100);
+    });
+
+    it('has a current health', () => {
+        expect(unit.attributes.currentHealth).toBe(60);
+    });
+
+    it('has an armor value', () => {
+        expect(unit.attributes.armor).toBe(100);
+    });
+
+    it('has a speed value', () => {
+        expect(unit.attributes.speed).toBe(10);
+    });
+
+    it('has a strength value', () => {
+        expect(unit.attributes.strength).toBe(10);
+    });
+
+    it('can have a spawn event handler', () => {
+        expect(unit.eventHandlers.spawn.length).toBeGreaterThan(0);
+    });
+
+    it('can have a death event handler', () => {
+        expect(unit.eventHandlers.spawn.length).toBeGreaterThan(0);
+    });
+
+    it('can be damaged', () => {
+        unit.damage(10);
+
+        expect(unit.attributes.currentHealth).toBe(50);
+    });
+
+    it('can die', (done) => {
+        unit.on('death', () => {
+            expect(unit.alive).toBe(false);
+
+            done();
+        });
+
+        unit.damage(100);
+    });
+
+    it('can be healed', () => {
+        unit.heal(10);
+
+        expect(unit.attributes.currentHealth).toBe(70);
+    });
+});
