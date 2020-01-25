9615b6ad43a7fb5bc8b0e22988256b94352a9877
Curtis <curtis@drumeo.com>
2020-01-25 15:08:20 -0800

    Add entry file

---
+++ index.js
@@ -0,0 +1,9 @@
+import BattleScene from './src/scene/battle-scene';
+import TextScene from './src/scene/text-scene';
+import Unit from './src/unit/unit';
+
+export default {
+    BattleScene,
+    TextScene,
+    Unit,
+};
